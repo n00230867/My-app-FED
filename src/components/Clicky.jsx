88eb552097908be794735f1ab3698d47facb8a22
Eduards Oss (Student) <N00230867@iadt.ie>
2025-09-22 16:34:59 +0100

    countdown created

--- src/components/Clicky.jsx
+++ src/components/Clicky.jsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 const Clicky = () => {
     const [count, setCount] = useState(0);
 
+    const handleClick = () => {
+        // count = count + 1;
+        setCount(count + 1);
+    };
+
     return (
         <>
             <h1>You have clicked the button { count } times.</h1>
---
+++ src/components/Countdown.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+export default function Countdown(){
+    
+    useEffect(() => {
+        
+    }, []);
+    
+    return (
+        <h3>Timer: x</h3>
+    );
+};
